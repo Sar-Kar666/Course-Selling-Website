869a809bdbcc120d41ccc3ac8e6435dfdfab4ddf
Sar-Kar666 <diptanusarkar699@gmail.com>
2025-07-20 14:38:46 +0530

    updated the ui

--- src/Components/Courses.tsx
+++ src/Components/Courses.tsx
@@ -20,7 +20,7 @@ export function Courses({ image, title, price, details }: CoursesProps) {
     }
 
     return <div > 
-        {openCourse && <div className="flex justify-center items-center" onClick={OnLoad} >
+        {openCourse && <div className="flex justify-center items-center" onClick={OnLoad}>
         <div className="max-h-85 max-w-110 rounded-2xl bg-white border-gray-400 shadow-lg cursor-pointer hover:scale-110 duration-300 ease-in-out">
         <div className="p-4">
             <div className=" rounded-2xl ">
@@ -42,7 +42,22 @@ export function Courses({ image, title, price, details }: CoursesProps) {
         </div>
     </div>}
 
-    {!openCourse && <Link to={"/course/details"} state={{ image, title, price, details }} ><CourseDetails/></Link>}
+    {!openCourse && <div className="top-0  left-0 flex justify-center items-center">
+    <div className=" bg-red-400 h-300">
+  <div className="">
+    {image}
+  </div>
+  <div>
+    {title}
+  </div>
+  <div>
+    {price}
+  </div>
+  <div>
+    {details}
+  </div>
+</div>
+</div>}
    
     </div> 
 }
